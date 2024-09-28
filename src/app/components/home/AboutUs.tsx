94b2f50f54f5ac069975a96d79c359a357091aca
Alejandro Giraldo Serna <giraldosernaalejandro16@gmail.com>
2024-09-27 19:25:46 -0500

    Home AboutUs

--- src/app/components/home/AboutUs.tsx
+++ src/app/components/home/AboutUs.tsx
@@ -8,7 +8,9 @@ function AboutUs() {
   return (
     <div className="md:h-[80vh] w-full bg-primaryDark text-white flex flex-col items-center justify-center p-8 md:justify-between md:gap-20 md:px-20 md:flex-row ">
       <div className="h-full flex flex-col items-center justify-center gap-8 md:w-1/2">
-        <h3 className="text-2xl text-center">Cómo actuamos</h3>
+        <h3 className="text-3xl uppercase text-center text-primarylight">
+          Cómo actuamos
+        </h3>
         <p className="text-justify">
           Nos enfocamos en proporcionarle a nuestros clientes la mejor atención
           en asesorías legales, brindando un acompañamiento permanente con
@@ -18,12 +20,12 @@ function AboutUs() {
           en tus procesos.
         </p>
       </div>
-      <div className="w-1/2 h-full max-h-full relative bg-neutral-200 rounded-lg">
+      <div className="w-1/2 h-full max-h-full bg-neutral-200 rounded-lg flex items-center justify-center">
         <Image
           src={ImgLawyer}
           alt="Img CEO SGM"
           fill={false}
-          className="h-full absolute bottom-0 left-0 object-contain"
+          className="h-full object-contain"
         />
       </div>
     </div>
